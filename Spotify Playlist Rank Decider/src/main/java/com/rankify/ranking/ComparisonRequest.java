113cdf00876130e5a8eb2c1475ef1824688ab99a
Ben Ackerman <brackerman@loyola.edu>
2026-07-01 09:15:02 -0400

    Deleting old directory

--- Spotify Playlist Rank Decider/src/main/java/com/rankify/ranking/ComparisonRequest.java
+++
@@ -1,6 +0,0 @@
-package com.rankify.ranking;
-
-import com.rankify.model.Song;
-
-/** A single "rank A vs B" question presented to the user. */
-public record ComparisonRequest(Song left, Song right) { }
